ac4a2a9036868e986c0d192e4e508091c241609c
Shivendra-11 <shivendrakeshari581@gmail.com>
2025-12-15 00:44:32 +0530

    update BASE_URL to include the API version path

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 // const BASE_URL = "http://localhost:4000/api/v1"
-const BASE_URL = "https://edtech-backend-4mdg.onrender.com"
+const BASE_URL = "https://edtech-backend-4mdg.onrender.com/api/v1"
 
 
 // AUTH ENDPOINTS
